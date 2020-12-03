044f46119f2147b1d0378100d914efceb8923aba
Erick Alejandro Nolasco Franco <eanolascof@cdmm.edu.mx>
2020-12-03 09:28:42 -0600

    A

---
+++ BusquedaBinaria/src/Prueba.java
@@ -0,0 +1,39 @@
+class BusquedaBinaria {
+
+public static int busquedaBin(double[] matriz, double valorBuscado) {
+
+if (matriz.length == 0) {
+return -1;
+}
+
+int mitad, inferior = 0;
+int superior = matriz.length-1;
+
+do {
+mitad = (inferior + superior) / 2;
+if (valorBuscado > matriz[mitad]) {
+inferior = mitad + 1;
+} else {
+superior = mitad-1;
+}
+} while (matriz[mitad] != valorBuscado && inferior <= superior);
+
+if (matriz[mitad] == valorBuscado) {
+return mitad;
+} else {
+return -1;
+}
+
+}
+
+}
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
